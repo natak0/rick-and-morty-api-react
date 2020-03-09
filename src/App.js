9ab682824bb29af79d615ebebf9e23be86598c2a
Natak0 <>
2020-03-09 11:49:09 +0100

    a list with api call results

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    //refer to the parent class constructor
+    super(props);
+    console.log(props);
+    //use this after parent call
+    this.state = {
+      error:null,
+      isLoaded:false,
+      info:[],
+      results:[]
+    }
+  }
+
+  //load data from api immediately after a component is mounted/inserted into the tree 
+  componentDidMount() {
+    const apiURL = "https://rickandmortyapi.com/api/character/?page=1";
+    fetch(apiURL)
+    .then(response => response.json())
+    .then((result) => {
+      console.log("RM INFO",result.info);
+      return result
+    })
+    .then(
+      //update the state
+      (result) => {
+        this.setState({
+          isLoaded:true,
+          info:result.info,
+          results:result.results
+        });
+        console.log("setstate", result)
+      },
+      //handle API errors here to separate them from bugs in
+      (error) => {
+        this.setState({
+          isLoaded:true,
+          error
+        });
+      }
+    )
+    }
+
+  render(){
+    const {error, isLoaded, info, results} = this.state;
+    console.log("STATE",results, isLoaded, error)
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } 
+    else if (!isLoaded){
+      return <div>Loading...</div>;
+    }
+    else {
+      return (
+        <ul>
+          {results.map(item => (
+            <li key={item.id}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      )
+    }
+  }
 }
 
 export default App;
