6975c54afa5ee81fbcbb85b1e0c6563b6e50795a
Natak0 <Natak0@example.com>
2020-03-24 18:07:50 +0100

    loading message manipulations

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -6,19 +6,6 @@ const character = ( props ) => (
 <div 
   className={"character-grid "+(props.selected?"selected":"")} 
   onClick={props.clicked}>
-    {/* <img 
-      className="character-poster"
-      src={props.image}
-      alt="character">
-    </img>
-    {(props.selected?
-    <div className="character-details-media">
-      <h1>{props.character.name}</h1>
-      <p>Gender: {props.character.gender}</p>
-      <p>Species: {props.character.species}</p>
-      <p>Status: {props.character.status}</p>
-      <p>Location: {props.character.location.name}</p>
-    </div>:null)} */}
     <CharacterDetails character={props.character} selected={props.selected}/>
 </div>
 )
--- src/dom/CharacterGridContainer.js
+++ src/dom/CharacterGridContainer.js
@@ -24,10 +24,15 @@ class CharacterGridContainer extends Component {
   }
 
   componentDidMount () {
-    this.apiFetch(this.state.apiURL);
+    this.setState({
+      isLoaded: false
+    }, () => {
+      this.apiFetch(this.state.apiURL);
+    })
+    
   }
-
   apiFetch = (apiURL) => {
+    //this.setState({isLoaded:false});
     fetch(apiURL)
       .then(
         //call the json function and return a promise
@@ -104,55 +109,58 @@ class CharacterGridContainer extends Component {
   
   render() {
       const {error, isLoaded} = this.state;
+      console.log(error, isLoaded);
       if (!isLoaded) {
         return <div>Loading...</div>;
       } 
-      else {
+      
           //render a map of characters from the state
           //add selected boolean to clicked element
           //assign a click event to the div with an image
-          const characters = this.state.characters.map((character) => {
-            return (
-              <Character
-                image={character.image}
-                selected={this.state.selectedCharacter.id===character.id?true:false}
-                clicked={() => this.characterSelectedHandler(character)}
-                character={character}/> 
-            )
-          }); 
-          return(
-            <div>
-              <div className="nav-top__search">
-                <a role="button" aria-label="home" onClick={() => {this.apiFetch(this.state.apiURLbase+'?page=1')}}><span className="home-icon" ></span></a>
-                <Search searchHandler={() => this.searchHandler} error={error}/>
-              </div>
-              {( error )? (<div className="nav-top__results">No results</div>):
-                <div>
-                    <div className="nav-top__pagination">
-                      <button disabled={!this.state.dataInfo.prev} onClick={() => this.apiFetch(this.state.dataInfo.prev)}>prev</button>
-                      <span className="current-page">{this.currentPageNumber(this.state.dataInfo.current, this.state.dataInfo.pages)}</span>
-                      <button disabled={!this.state.dataInfo.next} onClick={() => this.apiFetch(this.state.dataInfo.next)}>next</button>
-                    </div>
-                  <main id="main-content" className="content-main"> 
-                    <div id="character-container">
-                      <section id="character-container-grid">
-                        {characters}
-                      </section> 
-                    </div>
-                    <div className="sidebar">
-                      <div className="details">
-                        <CharacterDetails 
-                          character={this.state.selectedCharacter}/>
-                      </div> 
-                      <GenderGraph counts={this.genderCounter(characters)}/>   
-                    </div>
-                  </main>
-                </div>
-            }
-            </div>
+        const characters = this.state.characters.map((character) => {
+          return (
+            <Character
+              image={character.image}
+              selected={this.state.selectedCharacter.id===character.id?true:false}
+              clicked={() => this.characterSelectedHandler(character)}
+              character={character}/> 
           )
-        }
+        }); 
+        return(
+          <div>
+            {( !isLoaded )? (<p>Loading...</p>):<p></p>}
+            <div className="nav-top__search">
+              <a role="button" aria-label="home" onClick={() => {this.apiFetch(this.state.apiURLbase+'?page=1')}}><span className="home-icon" ></span></a>
+              <Search searchHandler={() => this.searchHandler} error={error}/>
+              {console.log(error, isLoaded)}
+            </div>
+            {( error )? (<div className="nav-top__results">No results</div>):
+              <div>
+                  <div className="nav-top__pagination">
+                    <button disabled={!this.state.dataInfo.prev} onClick={() => this.apiFetch(this.state.dataInfo.prev)}>prev</button>
+                    <span className="current-page">{this.currentPageNumber(this.state.dataInfo.current, this.state.dataInfo.pages)}</span>
+                    <button disabled={!this.state.dataInfo.next} onClick={() => this.apiFetch(this.state.dataInfo.next)}>next</button>
+                  </div>
+                <main id="main-content" className="content-main"> 
+                  <div id="character-container">
+                    <section id="character-container-grid">
+                      {characters}
+                    </section> 
+                  </div>
+                  <div className="sidebar">
+                    <div className="details">
+                      <CharacterDetails 
+                        character={this.state.selectedCharacter}/>
+                    </div> 
+                      <GenderGraph counts={this.genderCounter(characters)}/>   
+                  </div>
+                </main>
+              </div>
+          }
+          </div>
+        )
+      }
     }
-}
+
 
 export default CharacterGridContainer;
