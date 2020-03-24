8fac38edbc5cc166451c0384ff0f8041dc41bf57
Natak0 <Natak0@example.com>
2020-03-24 17:11:56 +0100

    nested character details inside character and updated media queries

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import CharacterDetails from '../CharacterDetails/CharacterDetails';
 
 const character = ( props ) => (
 //add selected class to the clicked element
-<div className={"character-grid "+(props.selected?"selected":"")} onClick={props.clicked}>
-     <img 
-            className="character-poster"
-            src={props.image}
-            alt="character">
-      </img>
-      {(props.selected?
-      <div className="character-details-media">
-        <h1>{props.character.name}</h1>
-        <p>Gender: {props.character.gender}</p>
-        <p>Species: {props.character.species}</p>
-        <p>Status: {props.character.status}</p>
-        <p>Location: {props.character.location.name}</p>
-      </div>:null)}
-
+<div 
+  className={"character-grid "+(props.selected?"selected":"")} 
+  onClick={props.clicked}>
+    {/* <img 
+      className="character-poster"
+      src={props.image}
+      alt="character">
+    </img>
+    {(props.selected?
+    <div className="character-details-media">
+      <h1>{props.character.name}</h1>
+      <p>Gender: {props.character.gender}</p>
+      <p>Species: {props.character.species}</p>
+      <p>Status: {props.character.status}</p>
+      <p>Location: {props.character.location.name}</p>
+    </div>:null)} */}
+    <CharacterDetails character={props.character} selected={props.selected}/>
 </div>
 )
  
--- src/Components/CharacterDetails/CharacterDetails.js
+++ src/Components/CharacterDetails/CharacterDetails.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
 
-class CharacterDetails extends Component {
-
-  render() {
-    let character = 
-      <div className="character-details">
-        <p>Select a character</p>
-      </div>
-
-    //if the character was clicked, display details of the clicked character
-    if ( this.props.character.id ){
-      character = 
-      <div className="character-details">
+const CharacterDetails = (props) => (
+      <div className="character-item">
         <img 
             className="character-poster"
             id="character-poster"
-            src={this.props.character.image}
+            src={props.character.image}
             alt="character">
         </img>
-        <h1>{this.props.character.name}</h1>
-        <p>Gender: {this.props.character.gender}</p>
-        <p>Species: {this.props.character.species}</p>
-        <p>Status: {this.props.character.status}</p>
-        <p>Location: {this.props.character.location.name}</p>
-		  </div>
-    }
-  return character;    
-  }
-}
+        <div className="character-details">
+          <h1>{props.character.name}</h1>
+          <p>Gender: {props.character.gender}</p>
+          <p>Species: {props.character.species}</p>
+          <p>Status: {props.character.status}</p>
+          <p>Location: {props.character.location.name}</p>
+        </div>
+      </div>
+)
 
 export default CharacterDetails;
